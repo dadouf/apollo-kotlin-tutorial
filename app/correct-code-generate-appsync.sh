a62c855a0942d5a6bf74bb112579410fb8345ce7
David Ferrand <d.ferrand@emogi.com>
2022-05-25 18:32:16 +0200

    Remove app sync stuff unused by apollo

--- app/correct-code-generate-appsync.sh
+++
@@ -1,1 +0,0 @@
-cd build/generated/source/appsync && LC_ALL=C && LANG=C && find . -type f | xargs sed -i '' 's/@Nullable @Nullable/@Nullable/g' && find . -type f | xargs sed -i '' 's/@Nonnull @Nonnull/@Nonnull/g'
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,12 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://plugins.gradle.org/m2/")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-        classpath("com.amazonaws:aws-android-sdk-appsync-gradle-plugin:3.3.1")
     }
 }
 
