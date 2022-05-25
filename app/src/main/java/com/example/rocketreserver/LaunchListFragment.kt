cb12c74f5f921a608950871b997bc01c16d33025
David Ferrand <d.ferrand@emogi.com>
2022-05-25 11:47:06 +0200

    Add a Create project button

--- app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
@@ -5,15 +5,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.amazonaws.util.DateUtils
 import com.apollographql.apollo.GraphQLCall
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import com.example.rocketreserver.databinding.LaunchListFragmentBinding
 import kotlinx.coroutines.channels.Channel
+import java.util.*
 
 class LaunchListFragment : Fragment() {
     private lateinit var binding: LaunchListFragmentBinding
@@ -29,7 +32,6 @@ class LaunchListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
 
         val projects = mutableListOf<GetAllProjectsQuery.Item>()
         val adapter = LaunchListAdapter(projects)
@@ -91,6 +93,35 @@ class LaunchListFragment : Fragment() {
 //
 //            adapter.onEndOfListReached = null
 //            channel.close()
+        }
+
+        binding.fab.setOnClickListener {
+            val date = Date()
+            apolloClient(requireContext()).mutate(
+                AddProjectMutation(
+                    "David test $date",
+                    "Mutation test from Android client",
+                    DateUtils.formatISO8601Date(date).substringBefore('T')
+                )
+            ).enqueue(object : GraphQLCall.Callback<AddProjectMutation.Data>() {
+                override fun onResponse(response: Response<AddProjectMutation.Data>) {
+                    if (response.hasErrors()) {
+                        Log.e(
+                            "SHITSHOW",
+                            "Couldn't create project: ${response.errors()?.get(0)?.message()}"
+                        )
+                        return
+                    }
+                    
+                    val msg = "Created project ID: ${response.data()?.createProjectById?.id}"
+                    Log.d("SHITSHOW", msg)
+                    Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show()
+                }
+
+                override fun onFailure(e: ApolloException) {
+                    Log.e("SHITSHOW", "Error", e)
+                }
+            })
         }
     }
 }
